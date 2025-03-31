a110c09bdd732aaa0ec00b914ab28fb4fb6abe04
EC2 Default User <ec2-user@ip-172-31-38-154.eu-north-1.compute.internal>
2025-03-31 20:28:52 +0000

    settings

--- my_furhat_backend/config/settings.py
+++ my_furhat_backend/config/settings.py
@@ -1,15 +1,20 @@
 import os
 from dotenv import load_dotenv, dotenv_values
+from pathlib import Path
+
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) 
+MOUNT_DIR = Path("/mnt")
+
 
 config = dotenv_values(".env")
 
 config.update({
-    "HF_HOME": "/mnt/models/caches/huggingface",
-    "TORCH_HOME": "/mnt/models/caches/torch",
-    "VECTOR_STORE_PATH": "/mnt/data/vector_store",
-    "DOCUMENTS_PATH": "/mnt/data/documents",
-    "MODEL_PATH": "/mnt/models/weights",
-    "CUDA_VISIBLE_DEVICES": "0"
+    "HF_HOME": os.getenv("HF_HOME", str(MOUNT_DIR / "hf_cache")),
+    "TORCH_HOME": os.getenv("TORCH_HOME", str(MOUNT_DIR / "torch_cache")),
+    "VECTOR_STORE_PATH": os.getenv("VECTOR_STORE_PATH", str(MOUNT_DIR / "vector_store")),
+    "DOCUMENTS_PATH": os.getenv("DOCUMENTS_PATH", str(MOUNT_DIR / "documents")),
+    "MODEL_PATH": os.getenv("MODEL_PATH", str(MOUNT_DIR / "models")),
+    "GGUF_MODELS_PATH": os.getenv("GGUF_MODELS_PATH", str(MOUNT_DIR / "models/gguf")),
+    "CUDA_VISIBLE_DEVICES": os.getenv("CUDA_VISIBLE_DEVICES", "0")
 })
